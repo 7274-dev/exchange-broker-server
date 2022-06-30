1ac8976b05834379a151a070a793be1943911e8f
Magiccop990 <jtulek@at-rt.com>
2022-06-30 18:40:22 +0200

    Add cors config

---
+++ src/main/kotlin/dev/the/mag/exchangebrokerbackend/config/GlobalCorsConfiguration.kt
@@ -0,0 +1,18 @@
+package dev.the.mag.exchangebrokerbackend.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class GlobalCorsConfiguration {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
+            }
+        }
+    }
+}
