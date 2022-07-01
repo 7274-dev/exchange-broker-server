5612fde7ec291879101439848ffa5135cfb00969
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2022-07-01 11:49:13 +0200

    Refactors methods to use response entity

--- src/main/kotlin/dev/the/mag/exchangebrokerbackend/controllers/AuthController.kt
+++ src/main/kotlin/dev/the/mag/exchangebrokerbackend/controllers/AuthController.kt
@@ -1,15 +1,13 @@
 package dev.the.mag.exchangebrokerbackend.controllers
 
+import dev.the.mag.exchangebrokerbackend.annotations.Authenticated
 import dev.the.mag.exchangebrokerbackend.dto.UserDto
-import dev.the.mag.exchangebrokerbackend.models.User
 import dev.the.mag.exchangebrokerbackend.models.toUserDto
-import dev.the.mag.exchangebrokerbackend.repositories.UserRepository
 import dev.the.mag.exchangebrokerbackend.request.RequestUser
 import dev.the.mag.exchangebrokerbackend.services.UserService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -22,11 +20,15 @@ class AuthController (
     val userService: UserService
     ) {
 
+    @PostMapping("/signin")
+    @Authenticated
+    fun signIn(): ResponseEntity<String> {
+        return ResponseEntity("OK", HttpStatus.OK)
+    }
 
     @PostMapping("/signup")
     fun signUp(@RequestBody user: RequestUser): ResponseEntity<UserDto> {
         val createdUser = userService.createNewUser(user.username, user.password, false, user.email);
         return ResponseEntity(createdUser.toUserDto(), HttpStatus.OK)
     }
-
 }
--- src/main/kotlin/dev/the/mag/exchangebrokerbackend/exceptionhandler/AuthExceptionHandler.kt
+++ src/main/kotlin/dev/the/mag/exchangebrokerbackend/exceptionhandler/AuthExceptionHandler.kt
@@ -5,23 +5,24 @@ import dev.the.mag.exchangebrokerbackend.exceptions.AccessDenied
 import dev.the.mag.exchangebrokerbackend.exceptions.AuthInvalid
 import dev.the.mag.exchangebrokerbackend.exceptions.AuthMissing
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class AuthExceptionHandler {
     @ExceptionHandler(AuthMissing::class)
-    fun handleAuthMissing(e: AuthMissing): ErrorDto {
-        return ErrorDto(HttpStatus.UNAUTHORIZED, e.message ?: "", e.message ?: "")
+    fun handleAuthMissing(e: AuthMissing): ResponseEntity<String> {
+        return ResponseEntity(e.message ?: "", HttpStatus.UNAUTHORIZED)
     }
 
     @ExceptionHandler(AuthInvalid::class)
-    fun handleAuthInvalid(e: AuthInvalid): ErrorDto {
-        return ErrorDto(HttpStatus.UNAUTHORIZED, e.message ?: "", e.message ?: "")
+    fun handleAuthInvalid(e: AuthInvalid): ResponseEntity<String> {
+        return ResponseEntity(e.message ?: "", HttpStatus.UNAUTHORIZED)
     }
 
     @ExceptionHandler(AccessDeniedException::class)
-    fun handleAccessDenied(e: AccessDenied): ErrorDto {
-        return ErrorDto(HttpStatus.FORBIDDEN, e.message ?: "", e.message ?: "")
+    fun handleAccessDenied(e: AccessDenied): ResponseEntity<String> {
+        return ResponseEntity(e.message ?: "", HttpStatus.FORBIDDEN)
     }
 }
