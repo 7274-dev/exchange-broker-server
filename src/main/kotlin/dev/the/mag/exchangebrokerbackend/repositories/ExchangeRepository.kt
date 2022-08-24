a92dcab991519706276e558d43a645fcfc054020
MagTheDev <49731971+MagTheDev@users.noreply.github.com>
2022-08-24 14:15:27 +0200

    Add functions to repo interfaces (#4)

--- src/main/kotlin/dev/the/mag/exchangebrokerbackend/repositories/ExchangeRepository.kt
+++ src/main/kotlin/dev/the/mag/exchangebrokerbackend/repositories/ExchangeRepository.kt
@@ -2,7 +2,11 @@ package dev.the.mag.exchangebrokerbackend.repositories
 
 import dev.the.mag.exchangebrokerbackend.models.Exchange
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Date
 
 interface ExchangeRepository : JpaRepository<Exchange, Long> {
     fun getExchangeByCode(code: Int): Exchange?
+    fun getExchangesByOwnerIdAndCloseDateIsBefore(ownerId: Long, closeDate: Date): List<Exchange>
+
+    fun getExchangesByOwnerId(ownerId: Long): List<Exchange>
 }
